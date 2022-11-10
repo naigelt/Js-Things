e755ae082da8a8753fc10e7752d0b69dbf73d87b
Niko Tossavainen <nikotossavainen1999@gmail.com>
2022-11-10 19:01:51 +0200

    Changes

---
+++ Learning_JS/example.js
@@ -0,0 +1,20 @@
+"use strict";
+console.log("Howdy partner!");
+
+alert("Hi from here too!");
+
+const name1 = "Pirjo";
+document.querySelector("#target").innerHTML = "Good morning, " + name1 + "!";
+
+let number, name;
+number = 153;
+name = "Anna";
+console.log(number);
+console.log(name);
+number = "-17";
+name = "Pekka";
+console.log(number);
+console.log(name);
+
+const name3 = "Ahmed";
+console.log(typeof name3);
